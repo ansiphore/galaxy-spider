308554d4f997829e5128f45dee304c584372aa92
Denis Gukov <denguk@gmail.com>
2021-10-02 02:42:58 +0500

    feat: init spider

--- galaxy_spider/spiders/website.py
+++ galaxy_spider/spiders/website.py
@@ -1,10 +1,36 @@
 import scrapy
+import sqlite3
 
 
 class WebsiteSpider(scrapy.Spider):
     name = 'website'
-    allowed_domains = ['ansible.galaxy.com']
-    start_urls = ['http://ansible.galaxy.com/']
+    allowed_domains = ['galaxy.ansible.com']
+
+    field_labels = {}
+
+    field_types = {}
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        conn = sqlite3.connect('result.db', isolation_level=None)
+        c = conn.cursor()
+
+        self.conn = conn
+
+    def start_requests(self):
+        for i in range(300):
+            yield scrapy.Request(
+                'https://galaxy.ansible.com/search?deprecated=false&keywords=&order_by=-relevance&page_size=100&page=' +
+                str(i + 1), self.parse)
 
     def parse(self, response):
+        if len(response.body) == 0:
+            return
+
+        role_links = response.css('.collection-item > .collection-container a')
+
+        for link in role_links:
+            yield response.follow(link, self.parse_role)
+
+    def parse_role(self, response):
         pass
